272383ede963dceca81fd0570d3e77c21fe97ce7
amiel103 <arj.nayve@gmail.com>
2026-02-02 01:46:27 +0800

    remove wrong answer

--- extract.py
+++ extract.py
@@ -6,9 +6,6 @@ def load_csv():
     """
     write a script that loads source csv data to sqllite file in the staging area 
     """
-
-
-    this is my answer,,,
     # conn = sqlite3.connect(sqlite_path)
     # df = pd.read_csv(csv_path)
     # df.to_sql(table_name, conn, if_exists='replace', index=False)
